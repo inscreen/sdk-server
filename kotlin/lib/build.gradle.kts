6d2c11d420df15a7ea85a97d2578b4736fc9c261
Ramon Snir <r@mon.dev>
2022-10-20 17:52:06 +0000

    OSSRH publishing

--- kotlin/lib/build.gradle.kts
+++ kotlin/lib/build.gradle.kts
@@ -1,11 +1,11 @@
-version = "0.1.1"
-
-group "com.github.inscreen"
+group = "com.inscreen"
+version = "0.1.1" // Update below too
 
 plugins {
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.serialization") version "1.7.10"
     `java-library`
+    `signing`
     `maven-publish`
 }
 
@@ -21,14 +21,61 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    withJavadocJar()
+    withSourcesJar()
+}
+
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("mavenJava") {
             groupId = "com.inscreen"
             artifactId = "sdkserver"
-            version = "0.0.0" // Doesn't apply.
+            version = "0.1.1" // Update above too
+
+            pom {
+                name.set("inScreen server-side SDK")
+                description.set("An SDK for using inScreen for Java/Kotlin applications")
+                url.set("https://inscreen.com/")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/mit-license.php")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("inScreen")
+                        email.set("info@inscreen.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/inscreen/sdk-server.git")
+                    developerConnection.set("scm:git:ssh://github.com/inscreen/sdk-server.git")
+                    url.set("https://github.com/inscreen/sdk-server/")
+                }
+            }
 
             from(components["java"])
         }
+
+        repositories {
+            maven {
+                name = "OSSRH"
+                setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = System.getenv("MAVEN_USERNAME")
+                    password = System.getenv("MAVEN_PASSWORD")
+                }
+            }
+        }
     }
+}
+
+signing {
+    useInMemoryPgpKeys(System.getenv("MAVEN_GPG_KEY"), System.getenv("MAVEN_GPG_PASSPHRASE"))
+    sign(publishing.publications)
 }
